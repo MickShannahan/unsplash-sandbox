5ce93887194daffb79e861793407c5274192577f
mick shannahan <beasticorn@gmail.com>
2022-03-15 22:03:15 -0600

    don't break my stuff

--- client/app/Services/AxiosService.js
+++
@@ -1,8 +0,0 @@
-import { baseURL } from '../env.js'
-
-/* eslint-disable no-undef */
-// @ts-ignore
-export const api = axios.create({
-  baseURL,
-  timeout: 8000
-})
--- client/app/main.js
+++
@@ -1,8 +0,0 @@
-import { ValuesController } from './Controllers/ValuesController.js'
-
-class App {
-  valuesController = new ValuesController();
-}
-
-// @ts-ignore
-window.app = new App()
