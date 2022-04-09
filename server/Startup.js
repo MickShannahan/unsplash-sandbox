4dd37553d45ce45aab08451ee62ac173f216e47a
mick shannahan <beasticorn@gmail.com>
2022-04-08 23:56:31 -0600

    i though i let in 8080

--- server/Startup.js
+++ server/Startup.js
@@ -16,7 +16,7 @@ export default class Startup {
   }
 
   static configureCors(app) {
-    const allowedDomains = ['http://localhost', '*']
+    const allowedDomains = ['http://localhost:8080', '*']
     const corsOptions = {
       origin(origin, callback) {
         if (process.env.NODE_ENV === 'dev') {
