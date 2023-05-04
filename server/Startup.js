9fe5e1a5fa419fd6eef0671f9b1d5f96926bb788
MickShannahan <63372695+MickShannahan@users.noreply.github.com>
2023-05-04 17:00:21 -0600

    Update Startup.js
    return

--- server/Startup.js
+++ server/Startup.js
@@ -22,7 +22,7 @@ export default class Startup {
         if (process.env.NODE_ENV === 'dev') {
           return callback(null, true)
         }
-        if(!origin) callback(null, true) // server to server
+        if(!origin) return callback(null, true) // server to server
         const originIsWhitelisted = allowedDomains.findIndex(ad => origin.includes(ad)) !== -1
         callback(null, originIsWhitelisted)
       },
