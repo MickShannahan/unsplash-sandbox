00cf25803e3b28a2c80d03b6e6c22a42f73d353f
MickShannahan <63372695+MickShannahan@users.noreply.github.com>
2023-05-17 16:55:29 -0600

    Update bgImage.js

--- server/models/bgImage.js
+++ server/models/bgImage.js
@@ -1,10 +1,10 @@
 const urlFormat = 'https://images.unsplash.com/photo-[*]&cs=tinysrgb&crop=entropy&fit=crop&fm=jpgq=80'
 export class BgImage {
   constructor(data) {
-    this.full = this.formatUrl(data.urls.full)
-    this.large = this.formatUrl(data.urls.raw)
-    this.medium = this.formatUrl(data.urls.regular)
-    this.small = this.formatUrl(data.urls.small)
+    this.full = data.urls.full
+    this.large = data.urls.raw
+    this.medium = data.urls.regular
+    this.small = data.urls.small
     this.photographer = data.user.name
     this.portfolio = data.user.links.html
     this.description = data.description || 'no description'
